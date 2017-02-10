b79a110559243a783347528be17e4de3bccbeed7
Breno Wruck Schneider <brenows@gmail.com>
2017-02-10 18:51:42 -0200

    Deadline message color is dynamic now. Changed the edit icon to view icon on finished tasks list and removed the check button.

--- app/components/tasks/controller/TasksCtrl.js
+++ app/components/tasks/controller/TasksCtrl.js
@@ -2,14 +2,57 @@
     "use strict";
     angular
         .module("letsDoIt")
-        .controller("TasksCtrl",
-            ["TaskService", TasksCtrl]);
+        .controller("TasksCtrl", ["TaskService", TasksCtrl]);
 
     function TasksCtrl(TaskService) {
-        
-        var vm = this;
-        vm.unfinishedTasks = TaskService.unfinishedTasks(); 
-        vm.finishedTasks = TaskService.finishedTasks(); 
 
+        var vm = this;
+        vm.unfinishedTasks = TaskService.unfinishedTasks();
+        CustomizeTasks(vm.unfinishedTasks);
+
+        vm.finishedTasks = TaskService.finishedTasks();
+
+    }
+
+    function CustomizeTasks(tasks) {
+        tasks.forEach(function (task) {
+            if (task.deadline) {
+                task.deadlineInDaysClass = deadlineClass(task.deadlineInDays);
+
+                task.deadlineInDays = deadlineName(task.deadlineInDays);
+            };
+        });
+    }
+
+    function deadlineName(deadlineInDays) {
+
+        if (deadlineInDays == 0) {
+            return "Today";
+        }
+        
+        if (deadlineInDays == 1) {
+            return deadlineInDays + " day";
+        }
+
+        return deadlineInDays + " days";
+
+
+    }
+
+    function deadlineClass(deadlineInDays) {
+        if (deadlineInDays <= -1) {
+            return "ui red label";
+        }
+
+        if (deadlineInDays == 0) {
+            return "ui orange label";
+        }
+
+        if (deadlineInDays <= 5) {
+            return "ui yellow label";
+        }
+        if (deadlineInDays > 5) {
+            return "ui green label";
+        }
     }
 }());
--- app/components/tasks/services/TaskService.js
+++ app/components/tasks/services/TaskService.js
@@ -18,7 +18,7 @@
                 "title": "Code the menu",
                 "description": "Set the menu options and references",
                 "registrationDate": new Date("02/09/2017"),
-                "deadline": new Date("02/11/2018"),
+                "deadline": new Date("02/11/2010"),
                 "done": false,
             },
             {
@@ -26,7 +26,7 @@
                 "title": "Finish the tasks list layout",
                 "description": "The task list needs to be revised",
                 "registrationDate": new Date("02/09/2017"),
-                "deadline": new Date("02/15/2017"),
+                "deadline": new Date("02/10/2017"),
                 "done": false,
             },
             {
@@ -108,7 +108,7 @@
 
             if (task.deadline) {
                 
-                var timeDiff = Math.abs(task.deadline.getTime() - Date.now());
+                var timeDiff = task.deadline.getTime() - Date.now();
                 var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
                 task.deadlineInDays = diffDays;
