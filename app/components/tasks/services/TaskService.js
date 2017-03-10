1be1a8901fe2cd6002e9d42fbb1c21fd4e6e1fc9
Breno Wruck Schneider <brenows@gmail.com>
2017-03-10 14:33:10 -0300

    Finished and Unfinished tasks is now working properly.

--- app/components/tasks/services/TaskService.js
+++ app/components/tasks/services/TaskService.js
@@ -7,31 +7,17 @@
 
     function TaskService($resource) {
        
+       //The API runs locally for now. 
        var apiUrl = 'http://localhost:4000/tasks';
        
        return {
            unfinishedTasks : $resource(apiUrl, {}, {
-               query : {method: 'GET', params: {}, isArray : true}
+               query : {method: 'GET', params: {finished : false}, isArray : true}
            }),
            finishedTasks : $resource(apiUrl, {}, {
-               query : {method: 'GET', params: {}, isArray : true}
+               query : {method: 'GET', params: {finished : true}, isArray : true}
            })
        } 
     }
-
-/*
-  angular.module('myApp.services', ['ngResource']).
-  factory("geoProvider", function($resource) {
-    return {
-      states: $resource('../data/states.json', {}, {
-        query: { method: 'GET', params: {}, isArray: false }
-      }),
-      countries: $resource('../data/countries.json', {}, {
-        query: { method: 'GET', params: {}, isArray: false }
-      })
-    };
-  });
-*/
-
 
 }());
