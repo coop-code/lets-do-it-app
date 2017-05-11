bca84b2e5dd4dc0016164d2c5d5a84ef420ee394
Ygor Kiefer <kiefer.ygor@gmail.com>
2017-05-11 13:18:49 -0300

    Fixed values not being set initially.
    Had to use "taskValue()" instead of "taskValue".
    Using $inject also didn't work and couldn't find a good guide about best
    practices regarding value definition...yet. For now, using a function to
    return the actual object. 
    
    This was causing a glitch where, instead of receiving the value defined
    in task.value.js, which was " [] ", it was receiving "undefined"
    instead.

--- app/values/task.value.js
+++ app/values/task.value.js
@@ -1,7 +1,7 @@
 //Values for tasks real time exhibition
 (function () {
 	'use strict';
-	angular.module('letsDoIt').value('tasksValue', taskValue);
+	angular.module('letsDoIt').value('tasksValue', taskValue());
 	
 	function taskValue() {
 		return {
